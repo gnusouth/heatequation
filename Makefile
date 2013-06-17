9bb4cfb0be187829b26d418f9dcd46e6293fb300
Michael Sproul <micsproul@gmail.com>
2013-06-17 21:20:03 +1000

    Parallel version. Slightly faster, needs a bit of work.

--- Makefile
+++ Makefile
@@ -1,17 +1,24 @@
 CC=clang
-CFLAGS=-W -Wall -pedantic -std=gnu99 -g
+CFLAGS=-W -Wall -pedantic -O2 -std=gnu99
 LDFLAGS=-lm -lpthread
 
 BINARIES=heateq
 
-.PHONY: all clean debug plot
+.PHONY: all clean debug sequential slow plot
 
 all: $(BINARIES)
 
 heateq: heateq.c
+	$(CC) $(CFLAGS) -g -o heateq heateq.c $(LDFLAGS)
 
 debug:
 	$(CC) $(CFLAGS) -D_DEBUG -o heateq heateq.c $(LDFLAGS)
+
+sequential:
+	$(CC) $(CFLAGS) -g -D_SEQUENTIAL -o heateq heateq.c
+
+slow:
+	$(CC) -W -Wall -pedantic -O0 -std=gnu99 -o heateq heateq.c
 
 plot:
 	@echo "Press [Enter] to finish"
--- heateq.c
+++ heateq.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <math.h>
 #include <string.h>
+#include <pthread.h>
 
 /* ~~~~~~~~~ */
 /* Constants */
@@ -50,6 +51,24 @@ int height = DEFAULT_HEIGHT;
 
 /* The number of iterations to perform */
 int num_iterations = ITERATIONS;
+
+/* The coefficient used in each computation */
+double coeff;
+
+/* A barrier to synchronise threads after each iteration */
+pthread_barrier_t iteration_barrier;
+
+/* ~~~~~~~ */
+/* Structs */
+/* ~~~~~~~ */
+
+/* A struct for dividing the work evenly between threads */
+struct work_chunk
+{
+	int start;
+	int end;
+	int id;
+};
 
 /* ~~~~~~~~~~~~ */
 /* Program Body */
@@ -140,83 +159,30 @@ void assign_initial_temps()
 	{
 		for (int j = 0; j < width/2; j++)
 		{
-			current(i, j) = 550.5;
 			previous(i, j) = 550.5;
 		}
 		for (int j = width/2; j < width; j++)
 		{
-			current(i, j) = 200.2;
 			previous(i, j) = 200.2;
 		}
 	}
 
 	/* Make the corners room temp? */
-	current(0,0) = 300;
-	current(0, width - 1) = 300;
-	current(height - 1, 0) = 300;
-	current(height - 1, width - 1) = 300;
+	previous(0,0) = 300;
+	previous(0, width - 1) = 300;
+	previous(height - 1, 0) = 300;
+	previous(height - 1, width - 1) = 300;
 }
 
-
-/* ~ The Main function ~*/
-
-int main(int argc, char ** argv)
+#ifdef _SEQUENTIAL
+/* Calculate the system's evolution sequentially */
+void sequential_calc()
 {
-	/* Read commandline arguments */
-	parse_args(argc, argv);
-
-	printf("~~~ heateq, by Michael Sproul. 2013 ~~~\n");
-
-	/* Calculate the differential equation coefficient, alpha */
-	const double alpha = (CONDUCTIVITY/(DENSITY*SPEC_HEAT_CAP));
-
-	/* TODO: Add the ability to change the material properties */
-
-	/* If delta-t is too large the solution will be unstable! */
-	{
-		double dt_limit = (delta_s*delta_s)/(2*alpha);
-
-		if (delta_t >= dt_limit)
-		{
-			printf("Please set a time interval less than %.5lf seconds\n",
-								 dt_limit);
-			exit(EXIT_FAILURE);
-		}
-	}
-
-	/* Create two arrays of temperature values, current and previous */
-	u_current = malloc(width*height*sizeof(double));
-	u_previous = malloc(width*height*sizeof(double));
-
-	if (u_current == NULL || u_previous == NULL)
-	{
-		perror("malloc");
-		exit(EXIT_FAILURE);
-	}
-
-	assign_initial_temps();
-
-	output_grid("initial.dat");
-
-	/* Calculate the discretised heat equation coefficient */
-	const double coeff = (alpha*delta_t)/(delta_s*delta_s);
-
-	double sum = 0;
-
-	printf("Material size: %dmm x %dmm\n", width, height);
-	printf("α: %.6lf m²/s, Δt: %.5lf s\n", alpha, delta_t);
-	printf("Iterations: %d\n", num_iterations);
-	printf("Calculating...");
-
 	/* Run! */
 	int t = 0;
+	double sum = 0;
 	for (t = 0; t < num_iterations; t++)
 	{
-		/* Make the current temps the previous ones */
-		double * temp = u_previous;
-		u_previous = u_current;
-		u_current = temp;
-
 		/* Recalculate the temperature at each position */
 		for (int i = 0; i < height; i++)
 		{
@@ -243,7 +209,169 @@ int main(int argc, char ** argv)
 				current(i, j) = previous(i,j) + coeff*sum;
 			}
 		}
+
+		/* Make the current temps the previous ones */
+		double * temp = u_previous;
+		u_previous = u_current;
+		u_current = temp;
 	}
+
+	/* After the final run, keep the temps */
+	double * temp = u_previous;
+	u_previous = u_current;
+	u_current = temp;
+}
+#endif
+
+#ifndef _SEQUENTIAL
+/* The thread function for parallel computation */
+void *thread_calc(void * v_work)
+{
+	struct work_chunk * work = (struct work_chunk *) v_work;
+	int start = work->start;
+	int end = work->end;
+	int id = work->id;
+
+	int sum = 0;
+
+	for (int t = 0; t < num_iterations; t++)
+	{
+		/* Calculate */
+		for (int i = start; i < end; i++)
+		{
+			for (int j = 0; j < width; j++)
+			{
+				sum = 0;
+
+				/* Add the second y derivative, if it exists */
+				if (has_y_deriv(i,j))
+				{
+					sum +=  previous(i + 1, j) +
+						previous(i - 1, j) -
+						2*previous(i,j);
+				}
+
+				/* Add the second x derivative, if it exists */
+				if (has_x_deriv(i,j))
+				{
+					sum +=  previous(i, j + 1) +
+						previous(i, j - 1) -
+						2*previous(i, j);
+				}
+
+				current(i, j) = previous(i,j) + coeff*sum;
+			}
+		}
+
+		/* Wait for other threads */
+		pthread_barrier_wait(&iteration_barrier);
+
+		/* Once per round maintenance */
+		if (id == 0)
+		{
+			/* Score swap */
+			double * temp = u_previous;
+			u_previous = u_current;
+			u_current = temp;
+		}
+	}
+
+	return NULL;
+}
+#endif
+
+/* ~ The Main function ~ */
+int main(int argc, char ** argv)
+{
+	/* Read commandline arguments */
+	parse_args(argc, argv);
+
+	printf("~~~ heateq, by Michael Sproul. 2013 ~~~\n");
+
+	/* Calculate the differential equation coefficient, alpha */
+	const double alpha = (CONDUCTIVITY/(DENSITY*SPEC_HEAT_CAP));
+
+	/* TODO: Add the ability to change the material properties */
+
+	/* If delta-t is too large the solution will be unstable! */
+	{
+		double dt_limit = (delta_s*delta_s)/(2*alpha);
+
+		if (delta_t >= dt_limit)
+		{
+			printf("Please set a time interval less than %.5lf seconds\n",
+								 dt_limit);
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	/* Create two matricies of temperature values, current and previous */
+	u_current = malloc(width*height*sizeof(double));
+	u_previous = malloc(width*height*sizeof(double));
+
+	if (u_current == NULL || u_previous == NULL)
+	{
+		perror("malloc");
+		exit(EXIT_FAILURE);
+	}
+
+	/* Set up the plate with some temperatures */
+	assign_initial_temps();
+	output_grid("initial.dat");
+
+	printf("Material size: %dmm x %dmm\n", width, height);
+	printf("α: %.6lf m²/s, Δt: %.5lf s\n", alpha, delta_t);
+	printf("Iterations: %d\n", num_iterations);
+	printf("Calculating...");
+
+	/* Calculate the discretised heat equation coefficient */
+	coeff = (alpha*delta_t)/(delta_s*delta_s);
+
+	#ifndef _SEQUENTIAL
+	/* Parallel Solution */
+	int nthreads = 4;
+
+	int quota = floor(1.00*height/nthreads);
+
+	pthread_t threads[nthreads];
+	struct work_chunk work[nthreads];
+
+	for (int i = 0; i < nthreads; i++)
+	{
+		work[i].start = i*quota;
+		work[i].end = (i + 1)*quota;
+		work[i].id = i;
+	}
+
+	/* Set the last one to cover the remainder */
+	work[nthreads - 1].end = height;
+
+	/* Initialise the thread barrier */
+	pthread_barrier_init(&iteration_barrier, NULL, nthreads);
+
+	/* Create threads */
+	for (int i = 0; i < nthreads; i++)
+	{
+		pthread_create(&threads[i], NULL, thread_calc, (void *)(work + i));
+	}
+
+	for (int i = 0; i < nthreads; i++)
+	{
+		pthread_join(threads[i], NULL);
+	}
+
+	/* Make u_current the actual current temps (unswap) */
+	double * temp = u_previous;
+	u_previous = u_current;
+	u_current = temp;
+
+	/* Destroy the thread barrier */
+	pthread_barrier_destroy(&iteration_barrier);
+	#endif
+
+	#ifdef _SEQUENTIAL
+	sequential_calc();
+	#endif
 
 	printf(" [done]\n");
 
